5209e69d56e255ddf6d4e5f9616fc72081c08af9
Joshua Farin <farin.k.joshua@gmail.com>
2020-06-02 20:46:01 -1000

    Enemy data

--- src/Script/Data/Location.java
+++ src/Script/Data/Location.java
@@ -1,4 +1,27 @@
 package Script.Data;
 
+import org.rspeer.runetek.api.movement.position.Area;
+
 public enum Location {
+    LUMBRIDGE_CHICKEN_PEN(Area.rectangular(3228, 3300, 3231, 3295), Enemy.CHICKEN),
+    LUMBRIDGE_SWAMP(Area.rectangular(3212, 3186, 3221, 3176), Enemy.GIANT_RAT, Enemy.GIANT_FROG),
+    AL_KHARID_PALACE(Area.rectangular(3287, 3170, 3298, 3167), Enemy.AL_KHARID_WARRIOR),
+    EDGEVILLE_DUNGEON(Area.rectangular(3113, 3453, 3117, 3450), Enemy.HILL_GIANT)
+    ;
+
+    private Area trainingArea;
+    private Enemy[] enemies;
+
+    Location(Area trainingArea, Enemy... enemies) {
+        this.trainingArea = trainingArea;
+        this.enemies = enemies;
+    }
+
+    public Area getTrainingArea() {
+        return trainingArea;
+    }
+
+    public Enemy[] getEnemies() {
+        return enemies;
+    }
 }
