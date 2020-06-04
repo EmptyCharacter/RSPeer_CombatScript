3109cca9cab31afc24d7daacd30c8dfdb07f63d5
Joshua Farin <farin.k.joshua@gmail.com>
2020-06-03 16:19:11 -1000

    set up GUI

--- src/Script/UI/GUI.java
+++ src/Script/UI/GUI.java
@@ -1,14 +1,44 @@
 package Script.UI;
 
+import Script.Data.Enemy;
+import Script.Data.Location;
+import net.miginfocom.swing.MigLayout;
+import org.rspeer.runetek.api.Game;
 import org.rspeer.script.task.Task;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends Task {
 
     private boolean validate;
 
+    private JComboBox enemyComboBox;
+    private JComboBox locationsComboBox;
+    private JButton startBtn;
+
     private JFrame frame;
+
+    public GUI(){
+        frame = new JFrame("Combat Script v1.0");
+        frame.setLayout(new MigLayout());
+        frame.setPreferredSize(new Dimension(200,200));
+
+        //Checkboxes
+        enemyComboBox = new JComboBox(Enemy.values());
+        locationsComboBox = new JComboBox(Location.values());
+        startBtn = new JButton("Start");
+
+        frame.add(enemyComboBox, "wrap, growx");
+        frame.add(locationsComboBox, "wrap, growx");
+        frame.add(startBtn, "growx");
+
+        frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        frame.setLocationRelativeTo(Game.getCanvas());
+        frame.pack();
+
+        frame.setVisible(true);
+    }
 
     @Override
     public boolean validate() {
