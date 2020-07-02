51f58db0f4ffdce3e3acbd0726941d0d1cefabc5
Joshua Farin <farin.k.joshua@gmail.com>
2020-07-01 19:01:44 -1000

    Update Fighting.java

--- src/Script/Tasks/Fighting.java
+++ src/Script/Tasks/Fighting.java
@@ -4,6 +4,9 @@ import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.script.task.Task;
 
 public class Fighting extends Task {
+
+    private static final String ATTACK_ACTION = new String("Attack");
+
     @Override
     public boolean validate() {
         return Players.getLocal();
