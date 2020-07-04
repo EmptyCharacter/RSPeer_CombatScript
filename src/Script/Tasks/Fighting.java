04c3a2b1c825d657b84220766858c7b3c2143a56
Joshua Farin <farin.k.joshua@gmail.com>
2020-07-04 13:13:11 -1000

    Update Fighting.java

--- src/Script/Tasks/Fighting.java
+++ src/Script/Tasks/Fighting.java
@@ -1,20 +1,29 @@
 package Script.Tasks;
 
+import Script.CombatScript;
+import org.rspeer.runetek.adapter.scene.Npc;
+import org.rspeer.runetek.api.component.tab.Combat;
+import org.rspeer.runetek.api.scene.Npcs;
 import org.rspeer.runetek.api.scene.Players;
 import org.rspeer.script.task.Task;
 
 public class Fighting extends Task {
 
     private static final String ATTACK_ACTION = new String("Attack");
-K
+
     @Override
     public boolean validate() {
-
+        return !Players.getLocal().isAnimating() && !Players.getLocal().isMoving();
 
     }
 
     @Override
     public int execute()  {
+        Npc target = Npcs.getNearest(x -> x.getName().equals(CombatScript.enemy.getEnemyName()) && CombatScript.location.getTrainingArea().contains(x));
+
+        if(target != null){
+            target.interact(ATTACK_ACTION);
+        }
         return 1000;
     }
 }
