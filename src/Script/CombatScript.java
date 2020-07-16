7ad614dacc4d63fdd62d8c776482962439a77bba
Joshua Farin <farin.k.joshua@gmail.com>
2020-07-15 20:36:06 -1000

    Update Fighting.java

--- src/Script/CombatScript.java
+++ src/Script/CombatScript.java
@@ -5,9 +5,15 @@ import Script.Data.Food;
 import Script.Data.Location;
 import Script.Tasks.*;
 import Script.UI.GUI;
+import org.rspeer.runetek.api.commons.StopWatch;
+import org.rspeer.runetek.api.component.tab.Skill;
+import org.rspeer.runetek.api.component.tab.Skills;
+import org.rspeer.runetek.event.types.RenderEvent;
 import org.rspeer.script.ScriptMeta;
 import org.rspeer.script.task.TaskScript;
 import org.rspeer.ui.Log;
+
+import java.awt.*;
 
 @ScriptMeta(name = "Combat sScript", desc = "Kills NPC's", developer = "empty")
 public class CombatScript extends TaskScript {
@@ -16,17 +22,36 @@ public class CombatScript extends TaskScript {
     public static Location location;
     public static Food food;
 
+    private StopWatch runtime;
+    private int startXP;
+
     @Override
     public void onStart() {
+        Log.fine("Script Started");
+        runtime = StopWatch.start();
+        startXP = Skills.getExperience(Skill.ATTACK);
+
         submit(new GUI(),
-                new Traverse(),
-                new Fighting(),
                 new ToggleRun(),
-                new Eating());
+                new Eating(),
+                new Traverse(),
+                new Fighting());
     }
 
     @Override
     public void onStop() {
         Log.severe("Script Stopped");
     }
+
+    public void notify(RenderEvent renderEvent)
+    {
+        Graphics g = renderEvent.getSource();
+
+        int gainedXP = (Skills.getExperience(Skill.ATTACK) - startXP);
+
+        g.drawString("Runtime: " + runtime.toElapsedString(), 20, 20);
+        g.drawString("XP Gained: " + gainedXP, 20, 40);
+        g.drawString("XP Per Hour: " + runtime.getHourlyRate(gainedXP), 20, 60);
+    }
+
 }
